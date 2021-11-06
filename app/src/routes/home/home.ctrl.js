b38df9e15ff136d268cc43fabd4a82a669bf1039
Park GaYeon <gay0830@naver.com>
2021-11-07 02:06:03 +0900

    로그인 인증기능 만들기 in 서버

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,17 +1,45 @@
 "use strict"
 
-const home = (req, res) =>{
-    //루트경로로 들어오면 나는 ~한 기능을 해주겠다.
-    res.render("home/index"); //아예 홈폴더를 views로 지정해 주었기 때문에 이렇게 써도 됨
-    //ejs의 파일로 응답을 해 주겠다 render
-    //어떤 요청인지는 모르겠지만 이렇게 응답을 해주겠다
+const users = {
+    id : ["gay0830", "나개발", "김팀장"],
+    psword : ["1234", "1234","12345"],
+}
+
+const output = {
+    home : (req, res) =>{
+        //루트경로로 들어오면 나는 ~한 기능을 해주겠다.
+        res.render("home/index"); //아예 홈폴더를 views로 지정해 주었기 때문에 이렇게 써도 됨
+        //ejs의 파일로 응답을 해 주겠다 render
+        //어떤 요청인지는 모르겠지만 이렇게 응답을 해주겠다
+    },
+    login : (req, res) => {
+        res.render("home/login");
+    },//로그인 경로로 들어가면 그 클라이언트(페이지)에 서버가 전송하는 값을 받음
+    
 };
-const login = (req, res) => {
-    res.render("home/login");
-};//로그인 경로로 들어가면 그 클라이언트(페이지)에 서버가 전송하는 값을 받음
+
+const process = {
+    login:(req, res) => {
+        const id = req.body.id,
+        psword = req.body.psword;
+
+        if (users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword){
+                return res.json({
+                    success: true,
+                })
+            }
+        }
+        return res.json({
+            success: false,
+            msg : "로그인에 실패하셨습니다.",
+        });
+    }
+}
 
 module.exports = {
-    home, 
-    login
+    output,
+    process
 };//object형식으로 외부로 보내줌
 //object = { key : value}/ key를 하나만 넣어주면 key와 같은 value로 넘겨줌
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,10 +7,11 @@ const router = express.Router();
 //home.ctrl에서 보내준 모듈 받아오기 위해 선언
 const ctrl = require("./home.ctrl")
 
-router.get("/", ctrl.home);//루트 경로를 만드는 것. 파라미터로 콜백함수를 넣어줌
+router.get("/", ctrl.output.home);//루트 경로를 만드는 것. 파라미터로 콜백함수를 넣어줌
 
-router.get("/login", ctrl.login)
+router.get("/login", ctrl.output.login)
 //라우트의 기능을 하는 것들을 몽땅 가지고 옴
 //app.js는 아직 index.js를 알지 못하기 때문에 외부에서 사용할수 있도록 던져주어야함
+router.post("/login", ctrl.process.login)
 
 module.exports = router;
