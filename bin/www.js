6305e0cf8e4b76386042336bef875384d5f46c32
Park GaYeon <gay0830@naver.com>
2021-11-06 20:47:48 +0900

    "add commit" Please enter the commit message for your changes. Lines starting Please enter the commit message for your changes. Lines starting Please enter the commit message for your changes. Lines starting commit.Changes to be committed: C:/Users/asus/Desktop/node/login-lecture/.git/COMMIT_EDITMSG[+] [unix] (20:47 06/11/2021) 9,68 18%-- 끼워넣기 -- ? npm start 이게 뭐냐고 아니

--- bin/www.js
+++
@@ -1,9 +0,0 @@
-//서버를 띄워주는 코드
-"use strict"
-
-const app = require("../app");
-const PORT = 3000;
-
-app.listen(PORT, () =>{
-    console.log("서버가동")
-}); //listen이라는 명령어로 서버를 띄운다, 두번째 파라미터로 콜백함수르 띄운다
