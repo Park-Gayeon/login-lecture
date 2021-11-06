29643ae560a6a20dcf387839f579e4665b41633d
Park GaYeon <gay0830@naver.com>
2021-11-07 03:29:57 +0900

    User 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict"
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
     home : (req, res) =>{
@@ -17,20 +17,8 @@ const output = {
 
 const process = {
     login:(req, res) => {
-        const id = req.body.id,
-        psword = req.body.psword;
-
-        const users = UserStorage.getUsers("id","psword");
-        const response = {};
-        if (users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if(users.psword[idx] === psword){
-                response.success = true;
-                return res.json(response);
-            }
-        }
-        response.success = false;
-        response.msg = "로그인에 실패하셨습니다."
+        const user = new User(req.body);
+        const response = user.login();
         return res.json(response);
     },
 };
@@ -40,3 +28,6 @@ module.exports = {
     process
 };//object형식으로 외부로 보내줌
 //object = { key : value}/ key를 하나만 넣어주면 key와 같은 value로 넘겨줌
+
+
+
