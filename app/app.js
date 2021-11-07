b3d94aba099f52e1b64c160b1315aa3dda6752af
Park GaYeon <gay0830@naver.com>
2021-11-07 16:41:35 +0900

    일단은 대충 끼워 맞춤

--- app/app.js
+++ app/app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express(); //app이라는 변수에 express를 넣는다
+const path = require("path");
+
 //라우팅
 const home = require("./src/routes/home");//home을 명시해줌
 
@@ -10,6 +12,7 @@ const home = require("./src/routes/home");//home을 명시해줌
 app.set("views", "./src/views");
 app.set("view engine", "ejs"); //view engine을 ejs로
 app.use(express.static(`${__dirname}/src/public`));
+app.use(express.static(path.join(__dirname, 'src/views/home')));
 app.use(bodyParser.json());
 // URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우
 //제대로 인식되지 않는 문제 해결
